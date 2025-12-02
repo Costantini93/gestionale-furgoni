cb828b7221aecc4d5c83df2482c1cd72fb172c43
Alessandro <ale.costa93@hotmail.it>
2025-12-02 16:19:27 +0100

    Fix: Sostituito sqlite3 con better-sqlite3 per compatibilità deploy

--- config/database.js
+++ config/database.js
@@ -1,13 +1,50 @@
-const sqlite3 = require('sqlite3').verbose();
+const Database = require('better-sqlite3');
 const path = require('path');
 
 const dbPath = path.join(__dirname, '..', 'database.db');
-const db = new sqlite3.Database(dbPath, (err) => {
-  if (err) {
-    console.error('Errore connessione database:', err);
-  } else {
-    console.log('Connesso al database SQLite');
-  }
-});
 
-module.exports = db;
+// Create database connection with better-sqlite3
+const db = new Database(dbPath, { verbose: console.log });
+
+console.log('Connesso al database SQLite');
+
+// Wrapper per compatibilità con sqlite3 API
+const dbWrapper = {
+  run: function(sql, params, callback) {
+    try {
+      const stmt = db.prepare(sql);
+      const result = stmt.run(...(Array.isArray(params) ? params : [params].filter(Boolean)));
+      if (callback) callback(null, result);
+      return result;
+    } catch (err) {
+      if (callback) callback(err);
+      throw err;
+    }
+  },
+  
+  get: function(sql, params, callback) {
+    try {
+      const stmt = db.prepare(sql);
+      const result = stmt.get(...(Array.isArray(params) ? params : [params].filter(Boolean)));
+      if (callback) callback(null, result);
+      return result;
+    } catch (err) {
+      if (callback) callback(err);
+      throw err;
+    }
+  },
+  
+  all: function(sql, params, callback) {
+    try {
+      const stmt = db.prepare(sql);
+      const result = stmt.all(...(Array.isArray(params) ? params : [params].filter(Boolean)));
+      if (callback) callback(null, result);
+      return result;
+    } catch (err) {
+      if (callback) callback(err);
+      throw err;
+    }
+  }
+};
+
+module.exports = dbWrapper;
